1cbf8c5c9c5dbf931312d6a58989b99d8d81c74e
Kenzo Nirasawa <kemuzo.nira@gmail.com>
2017-04-30 20:32:54 +0900

    project名変更

--- SpaceCat-Bridging-Header.h
+++ SpaceCat-Bridging-Header.h
@@ -1,13 +1,13 @@
 //
-//  HitBackApp-Bridging-Header.h
-//  HitBackApp
+//  SpaceCat-Bridging-Header.h
+//  SpaceCat
 //
 //  Created by Kenzo on 2015/01/27.
 //  Copyright (c) 2015年 Kenzo. All rights reserved.
 //
 
-#ifndef HitBackApp_HitBackApp_Bridging_Header_h
-#define HitBackApp_HitBackApp_Bridging_Header_h
+#ifndef SpaceCat_SpaceCat_Bridging_Header_h
+#define SpaceCat_SpaceCat_Bridging_Header_h
 #import "GADBannerView.h"
 #import "GADInterstitial.h"
 #endif
